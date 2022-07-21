a423687f42000c1a2f74604f91b000fb5bff98f5
eliton <netinho_123_anps@hotmail.com>
2022-07-21 10:17:42 -0300

    Commit do código html // Criado as funções JS // CSS funcionando //

---
+++ js/flappy.js
@@ -0,0 +1,120 @@
+// function para criar tags/classes html
+function novoElemento(tagName, className){
+  const elem = document.createElement(tagName)
+  elem.className = className
+  return elem
+}
+// definindo a barreita e a ordem da borda.
+function Barreira(reversa = false){
+  this.elemento = novoElemento('div', 'barreira')
+
+  const borda = novoElemento('div', 'borda')
+  const corpo = novoElemento('div', 'corpo')
+  this.elemento.appendChild(reversa ? corpo : borda)
+  this.elemento.appendChild(reversa ? borda : corpo)
+
+  // definir altura da barreira
+  this.setAltura = altura => corpo.style.height = `${altura}px`
+}
+
+///////////////////// teste //////////////////////
+// const b = new Barreira(true)
+// b.setAltura(200)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+
+///////////// Criando uma nova div para as barreiras///////////
+function ParDeBarreiras(altura, abertura, x){
+  this.elemento = novoElemento('div', 'par-de-barreiras')
+// definindo se é inferior ou superior
+  this.superior = new Barreira(true)
+  this.inferior = new Barreira(false)
+// injetando no html
+  this.elemento.appendChild(this.superior.elemento)
+  this.elemento.appendChild(this.inferior.elemento)
+
+  this.sortearAbertura = () =>{
+    const alturaSuperior = Math.random() * (altura - abertura)
+    const alturaInferior = altura - abertura - alturaSuperior
+    this.superior.setAltura(alturaSuperior)
+    this.inferior.setAltura(alturaInferior)
+  }
+
+  //function para pegar a posição X da barreira
+  this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+
+  this.setX = x => this.elemento.style.left = `${x}px`
+  this.getLargura = () => this.elemento.clientWidth
+
+  this.sortearAbertura()
+  this.setX(x)
+}
+
+///////////////////////// teste ////////////////////
+  /// const b = new ParDeBarreiras(700, 200, 400) // 700 = altura da tela
+    /// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function Barreiras(altura, largura, abertura, espaco, notificarPonto){
+  this.pares = [
+    new ParDeBarreiras(altura, abertura, largura),
+    new ParDeBarreiras(altura, abertura, largura + espaco),
+    new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+    new ParDeBarreiras(altura, abertura, largura + espaco * 3)
+  ]
+  // deslocamento da barreira em pixels
+  const deslocamento = 3
+  this.animar = () =>{
+    this.pares.forEach(par =>{
+      par.setX(par.getX() - deslocamento)
+
+      // para quando siar da tela
+      if(par.getX() < -par.getLargura()){
+        par.setX(par.getX() + espaco * this.pares.length)
+        //sorteando novas barreiras
+        par.sortearAbertura()
+      }
+      const meio = largura / 2
+      const cruzouMeio = par.getX() + deslocamento >= meio && par.getX() < meio
+      if(cruzouMeio) notificarPonto()
+    })
+  }
+}
+function Passaro(alturaJogo){
+  let voando = false
+
+  this.elemento = novoElemento('img', 'passaro')
+  this.elemento.src = 'imgs/passaro.png'
+  
+  //posição do passaro
+  this.getY = () => parseInt(this.elemento.style.bottom.split('px'[0]))
+  this.setY = y => this.elemento.style.bottom = `${y}px`
+
+  window.onkeydown = e => voando = true
+  window.onkeyup = e => voando = false
+
+  this.animar = () =>{
+    const novoY = this.getY() + (voando ? 8 : -5)
+    const alturaMaxima = alturaJogo - this.elemento.clientHeight
+
+    // checagem para manter o passaro dentro da area do jogo
+    if(novoY <= 0){
+      this.setY(0)
+    }else if (novoY >= alturaMaxima){
+      this.setY(alturaMaxima)
+    }else{
+      this.setY(novoY)
+    }
+  }
+  this.setY(alturaJogo / 2)
+}
+
+const barreiras = new Barreiras(700,1200,200,400)
+const passaro = new Passaro(700)
+const areaDoJogo = document.querySelector('[wm-flappy]')
+
+areaDoJogo.appendChild(passaro.elemento)
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+setInterval(() =>{
+  barreiras.animar()
+  passaro.animar()
+}, 20)
